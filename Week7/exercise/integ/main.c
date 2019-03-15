3ece7a48f576d32cd1331faf0c5970302efef091
hasse302@hotmail.com <hasse302@hotmail.com>
2019-03-15 13:29:33 +0100

    integ and multiroot

--- Week7/exercise/integ/main.c
+++ Week7/exercise/integ/main.c
@@ -1,5 +1,72 @@
+#include<stdio.h>
 #include<math.h>
 #include<gsl/gsl_integration.h>
-#include<gsl/gsl_errno.h>
+
+double eq1 (double x, void * params){
+	double f=log(x)/sqrt(x);
+	return f;
+}
+
+double norm (double x, void * params){
+	double alpha = *(double *)params;
+	double norm=exp(-alpha * x * x);
+	return norm;
+}
+
+double hamil(double x, void * params){
+	double alpha = *(double *)params;
+	double hamil = exp(-alpha * x * x)*(-alpha*alpha*x*x/2 + alpha/2 + x*x/2);
+	return hamil;
+}
+
+
+int main(){
+	double epsabs=1e-6,epsrel=1e-6;
+	double E,alpha;
+
+
+	/* Integration of part 1 */
+	gsl_integration_workspace * Work = gsl_integration_workspace_alloc(100);
+	double results,errors;
+	gsl_function F;
+	F.function=&eq1;
+	F.params=0;
+
+	gsl_integration_qags(&F,0,1,epsabs,epsrel,100,Work,&results,&errors);
+	printf("The result of the integration is %lg \n",results);
+
+	double limit=5000;
+
+	/*integration of E = hamilton/norm */
+	gsl_integration_workspace * W_N =gsl_integration_workspace_alloc(limit);
+	gsl_integration_workspace * W_H =gsl_integration_workspace_alloc(limit);
+	
+	gsl_function Norm;
+	gsl_function Hamil;
+
+	Norm.function = &norm;
+	Hamil.function = &hamil;
+
+	Norm.params=&alpha;
+	Hamil.params=&alpha;
+
+	double result1,result2;
+	double error1,error2;
+	int i;
+	for(i=1;i<=300;i++){
+		alpha=i/100.0;
+		gsl_integration_qagi(&Norm,epsabs,epsrel,limit,W_N,&result1,&error1);
+		gsl_integration_qagi(&Hamil,epsabs,epsrel,limit,W_H,&result2,&error2);
+		E=result2/result1;
+	printf("%lg %lg \n", alpha, E);
+	}
+	gsl_integration_workspace_free(W_N);
+	gsl_integration_workspace_free(W_H);
+
+	return 0;
+
+}
+
+
 
 
---
+++ Week7/exercise/multiroot/main.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<gsl/gsl_vector.h>
+#include<gsl/gsl_multiroots.h>
+
+
+
+/* setting up a and b in f(x) = a*(1-x)^2 + b*(y-x^2)^2, where in our case, a = 1 and b = 100 */
+struct rparams{
+	double a;
+	double b;
+};
+
+
+/* now for the rosenbrock function */
+
+int rosenbrock_func(const gsl_vector * x, void * params, gsl_vector * f) {
+
+	double a = ((struct rparams *)params)->a;
+	double b = ((struct rparams *)params)->b;
+
+	const double x0 = gsl_vector_get(x,0);
+	const double x1 = gsl_vector_get(x,1);
+
+	/*so the first part of the eq: a * (1-x)^2 and the second part of the eq: b * (y - x^2)^2 */
+	const double y0 = a * (1-x0)*(1-x0);
+	const double y1 = b * (x1 - x0*x0)*(x1-x0*x0);
+	
+	gsl_vector_set(f,0,y0);
+	gsl_vector_set(f,1,y1);
+
+return GSL_SUCCESS;
+}
+
+int print_state(size_t iter, gsl_multiroot_fsolver *s){
+	printf("iter = %3u x = % .3f % .3f "
+			"f(x) = %.3f %.3e \n",
+			iter,
+			gsl_vector_get(s->x,0),
+			gsl_vector_get(s->x,1),
+			gsl_vector_get(s->f,0),
+			gsl_vector_get(s->f,1));
+}
+
+
+
+
+
+int main(void){
+
+	const gsl_multiroot_fsolver_type *T;
+	gsl_multiroot_fsolver *s;
+
+	int status;
+	size_t i, iter=0;
+	
+	/*setting a and b */
+	const size_t n= 2;
+	struct rparams p = {1.0,100.0};
+	gsl_multiroot_function f = {&rosenbrock_func,n,&p};
+	
+	/* setting up initial guess on the solution (1,1) */
+	double x_init[2]= {1,1};
+	gsl_vector *x = gsl_vector_alloc(n);
+
+	gsl_vector_set(x,0,x_init[0]);
+	gsl_vector_set(x,1,x_init[1]);
+
+	T=gsl_multiroot_fsolver_hybrids;
+	s=gsl_multiroot_fsolver_alloc(T,2);
+	gsl_multiroot_fsolver_set(s,&f,x);
+
+	print_state(iter,s);
+	
+	do {
+		iter++;
+		status = gsl_multiroot_fsolver_iterate(s);
+		print_state(iter,s);
+
+		if(status)
+			break;
+
+		status = gsl_multiroot_test_residual (s->f,1e-7);
+	}
+	while(status == GSL_CONTINUE && iter < 1000);
+	printf("status = %s\n",gsl_strerror(status));
+
+	gsl_multiroot_fsolver_free(s);
+	gsl_vector_free(x);
+
+
+
+
+return 0;
+}
