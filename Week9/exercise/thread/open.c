9e8c33f19150b69fda79279619261aa66914da09
hasse302@hotmail.com <hasse302@hotmail.com>
2019-03-27 16:42:17 +0100

    Added threading

--- Week9/exercise/thread/open.c
+++ Week9/exercise/thread/open.c
@@ -1,11 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#include<pthread.h>
 
 struct harm {int a,b;double result;};
-
-
 
 void* dart(void* param){
 	struct harm* data = (struct harm*)param;
@@ -29,27 +26,35 @@ return 0;
 }
 
 
+
 int main(int argc, char** argv){
-	int n = argc>1? (int)atof(argv[1]):1e6; /*NUMBER OF DARTS! CHANGE HERE */
+	int n = argc>1 ? (int)atof(argv[1]):1e6; /*NUMBER OF DARTS! CHANGE HERE */
 	int mid = n/2;
+	
 	struct harm part1,part2;
 
 	part1.a=0;
 	part1.b=mid;
-	pthread_t traad;
-	pthread_create(&traad,NULL,dart,(void*)&part1);
 
 	part2.a=mid;
 	part2.b=n;
+
+
+
+#pragma omp parallel sections
+   {
+   #pragma omp section  // first thread will run this block of code
+      {  
+	dart((void*)&part1);
+      }  
+   #pragma omp section  // second thread will run this block of code
+      {  
 	dart((void*)&part2);
-	pthread_join(traad,NULL);
-	
-	double pi =4*((double)part1.result+(double)part2.result)/n;
-	printf("Pi is approximately = %g \n",pi);
+      }  
+   }
 
-	
-	return EXIT_SUCCESS;
+	double pi=4*((double)part1.result+(double)part2.result)/n;
+	printf("Pi using OpenMD is approximately = %g \n",pi);
+
+return 0;
 }
-
-
-
