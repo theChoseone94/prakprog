30fcafa04a50afa4af1aa125d68ff1fdb600c315
hasse302@hotmail.com <hasse302@hotmail.com>
2019-04-05 16:07:01 +0200

    finished big programming exercise

--- Week10/exercise/big_programming_exercise/main.c
+++ Week10/exercise/big_programming_exercise/main.c
@@ -4,17 +4,19 @@
 #include<gsl/gsl_errno.h>
 #include<gsl/gsl_odeiv2.h>
 
-int myarctan(double x,const double y[],double yprime[],void * params){	
+int arctan_eq(double x,const double y[],double yprime[],void * params){	
 	yprime[0]=1/(x*x+1); /* set the diff eq here: atan=1/(x*x+1 )*/
 return GSL_SUCCESS;
 }
 
 
-int main(){
+
+double myarctan(double x){
+
 	
 	gsl_odeiv2_system ARCTAN;
 	
-	ARCTAN.function=myarctan;
+	ARCTAN.function=arctan_eq;
 	ARCTAN.dimension=1;
 
 	double hstart=1e-3,epsabs=1e-9,epsrel=1e-9;	
@@ -22,22 +24,23 @@ int main(){
 	gsl_odeiv2_driver * DRIVER=
 		gsl_odeiv2_driver_alloc_y_new(&ARCTAN,gsl_odeiv2_step_rkf45,hstart,epsabs,epsrel);
 
-
-	double delta_x=0.05,x_min=0,x_max=3;
 	double t=0;
 	double y[1]={0};
-	for(double x=x_min;x<x_max;x+=delta_x){
-		t=0;
-		y[0]=0;
-		int status = gsl_odeiv2_driver_apply(DRIVER,&t,x,y);
-		printf("%g %g %g \n",x,y[0],atan(x));
-		if(status != GSL_SUCCESS)
-			fprintf(stderr,"function:status=%i",status);
-	}
-
-
+	t=0;
+	y[0]=0;
+	double status = gsl_odeiv2_driver_apply(DRIVER,&t,x,y);
+	if(status != GSL_SUCCESS)
+		fprintf(stderr,"function:status=%g",status);
 
 	gsl_odeiv2_driver_free(DRIVER);
+return y[0];
+}
 
-return EXIT_SUCCESS;
+
+int main(){
+	
+	for(double x=0;x<3;x+=0.1)
+		printf("%g %g %g\n",x,myarctan(x),atan(x));
+
+return 0;
 }
