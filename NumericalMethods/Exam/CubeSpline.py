99d15546b7f928c235d8b66a1339531799915b87
hasse302@hotmail.com <hasse302@hotmail.com>
2019-06-06 16:18:09 +0200

    Exam project

--- NumericalMethods/Exam/CubeSpline.py
+++ NumericalMethods/Exam/CubeSpline.py
@@ -2,22 +2,67 @@ import numpy as np
 
 
 def CS_with_D(x,y,dydx):
-    b=np.zeros(np.size(dydx))
+    n=np.size(dydx)
+    h=np.zeros(np.size(dydx))
+    p=np.zeros(np.size(dydx))
+    for i in range(n-1):
+        h[i] = x[i+1]-x[i] #step size between points
+        p[i] = (y[i+1]-y[i])/h[i]
+        
+    b=np.array([])
     c=np.zeros(np.size(dydx))
     d=np.zeros(np.size(dydx))
     
+
+    b=np.append(b,p[0])
+    b=np.append(b,(p[0]+p[1])/2.0)
     
-    for i in range(np.size(x)-1):
-        b[i] = dydx[i]
-        c[i] = 3*(y[i+1]-y[i]) - 2*dydx[i] - dydx[i+1]
-        d[i] = 2*(y[i]-y[i+1]) + dydx[i] + dydx[i+1]
+    for i in range(2,n-2):
+        w1=np.fabs(p[i+1]-p[i])
+        w2=np.fabs(p[i-1]-p[i-2])
+        if(w1+w2==0):
+            b=np.append(b,(p[i-1]+p[i])/2.0)
+        else:
+            b=np.append(b,(w1*p[i-1]+w2*p[i])/(w1+w2))
     
     
-#calculating S
+    b=np.append(b,(p[n-2]+p[n-3])/2.0) #b[n-2]
+    b=np.append(b,p[n-2]) #b[n-1]
+    for i in range(2,np.size(x)-1):
+        c[i] = (3*(p[i]) - 2*b[i] - b[i+1])/(h[i])
+        d[i] = (-2*(p[i]) + b[i] + b[i+1])/(h[i])**2
+    
+    
+ 
 
-    S[i] = y[i] + b[i] * (x-x0[i]) + c[i]*(x-x0[i])**2 + d[i]*(x-x0[i])**3
+    #calculating S
+    x_p=np.linspace(0,2*np.pi+1,n)
+    S=np.zeros(np.size(dydx))
+    for i in range (np.size(x-1))   :
+        S[i] = y[i] + b[i] * (x_p[i]-x[i]) + c[i]*(x_p[i]-x[i])**2 + d[i]*(x_p[i]-x[i])**3
 
-
-
+    
 
     return S
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+#for i in range(n):
+#    b[0]=p[0]
+#    b[1]=(p[0]+p[1])/2
+#    b[n-1]=p[n-2]
+#    b[n-2]=(p[n-2]+p[n-3])/2
+#    
+#for i in range()
+#    
--- NumericalMethods/Exam/main.py
+++ NumericalMethods/Exam/main.py
@@ -1,9 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from CubeSpline import *
+plt.close('all')
+
+
+
 
 #The data set of sin(x) with derivative cos(x) from 0 to 2 pi
-x0=np.linspace(0,2*np.pi,20)#x_i in the assignment
+x0=np.linspace(0,2*np.pi+1,20)#x_i in the assignment
 y=np.sin(x0) #y_i in the assignment
 dydx=np.cos(x0) # y'_i in the assignment 
 
@@ -12,16 +16,14 @@ dydx=np.cos(x0) # y'_i in the assignment
 
 S=CS_with_D(x0,y,dydx)
 
-
-
-
 #Setting up the plot
-plt.figure()
 #plot the interpolated function
 
 
 
 #plot sin(x) and cos(x)
+plt.figure()
 plt.plot(x0,y,'ro',label='sin(x) from data')
 plt.plot(x0,dydx,'ko',label='sin\'(x) = cos(x) from data')
+plt.plot(x0,S,'g--',label='C-spline with derivative')
 plt.legend()
